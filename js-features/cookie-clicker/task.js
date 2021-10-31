eb54231a54db7d1f02d48d5ec11a2c8ec3c1698a
Artem Stepanyuk <aivanov757@gmail.com>
2021-10-31 10:42:48 +0300

    Второе задание

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -4,18 +4,14 @@ const image = document.getElementById('cookie');
 const bigsize = '500';
 const smallsize = '200';
 
-function changeSize() {
-    if (image.width == smallsize){
+let clicks = 0;    
+function myClick(){
+    clicks += 1;
+   const count = document.getElementById('clicker__counter');
+   count.textContent = clicks;
+   if (image.width == smallsize){
         image.width = bigsize;
     } else (image.width = smallsize);
 };
 
-image.onclick = changeSize
-
-let clicks = 0;    
-function myClick(){
-    clicks +=1;
-   const count = document.getElementById('clicker__counter');
-   count.textContent = clicks;
-
-};
+image.onclick = myClick
