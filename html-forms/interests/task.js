4c70f0f6751216402fe0f6b9ca2e7adbfc3abbe8
Artem Stepanyuk <aivanov757@gmail.com>
2022-04-06 10:41:34 +0300

    Готовый вариант

---
+++ html-forms/interests/task.js
@@ -0,0 +1,24 @@
+let checkboxArr = document.querySelectorAll('.interest__check');
+let itemsArr = document.querySelectorAll('.interest_active');
+
+
+
+checkboxArr.forEach((elem)=> {
+    elem.addEventListener('click',()=>{
+        if (elem.closest('.interests_active') == null) {
+            // console.log(elem.parentElement);            
+            elem.parentElement.nextElementSibling.querySelectorAll('input').forEach(el => {
+                if (elem.checked) {
+                    el.checked = true
+                } else {
+                    el.checked = false
+                }
+            })
+
+        // console.log('Жмакнули родича')
+        
+        }
+    })  
+})
+
+
