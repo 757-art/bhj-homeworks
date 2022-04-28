2eb320029e4d2e0a921b1fe4f9a27a6480b51ffc
Artem Stepanyuk <aivanov757@gmail.com>
2022-04-28 18:53:10 +0300

    Готовый вариант2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,32 +1,29 @@
 let newTaskField = document.querySelector('.tasks__input');
 let taskList = document.querySelector('.tasks__list');
 let form = document.getElementById('tasks__form');
-let remover = document.querySelectorAll('.task__remover');
+let tasks = document.querySelectorAll('.task');
 
 form.onsubmit = function() {
     let text = newTaskField.value;
-        if (text != '') {
-            taskList.innerHTML += `
-        <div class="task">
-          <div class="task__title">${text}</div>
-          <a href="#" class="task__remove">
-            &times;
-          </a>
-        </div>
-      `;
-      let newTask = document.querySelectorAll('.task');
-      if (newTask) {
-      newTask.forEach((elem) => {
-          elem.addEventListener('click', function(){
-              remover.onclick
-              elem.remove();
-          })
-      })
-  }
+    if (text != '') {
+        taskList.insertAdjacentHTML("beforeend", `<div class="task">
+        <div class="task__title">${text}</div>
+        <a href="#" class="task__remove">
+        &times;
+        </a>
+        </div>`)
+        let remover = document.querySelectorAll('.task__remove');
+        for (let i = 0; i < remover.length; i++) {
+            i = remover[remover.length -1] 
+            i.addEventListener('click', function(){
+                i.parentElement.remove();
+            })
+        }
+    
+          
     }
     newTaskField.value = '';
     return false;
-  };
+};
 
- 
 
