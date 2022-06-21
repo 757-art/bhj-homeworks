3fd2b4fad1a7311df03fdee0d59cd03e6620a393
Artem Stepanyuk <aivanov757@gmail.com>
2022-06-21 10:46:37 +0300

    Готовый вариант2

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,20 +1,25 @@
-let xhr = new XMLHttpRequest();
-let loader = document.getElementById('loader');
-let itemsContainer = document.getElementById('items');
+const xhr = new XMLHttpRequest();
+const loader = document.getElementById('loader');
+const itemsContainer = document.getElementById('items');
 
 
 xhr.open('GET', ' https://netology-slow-rest.herokuapp.com')
+
+xhr.responseType = 'json';
+
 xhr.send();
 
-xhr.onreadystatechange = function() {
-   
-    if (xhr.readyState === 4) {
-        loader.classList.remove('loader_active')
-        let items = JSON.parse(xhr.responseText).response.Valute;
-        let itemsArr = Object.values(items);
-            console.log(Object.values(items))
-        //    let itemElem = document.querySelector('.item');   
-        itemsArr.forEach(elem => {
+
+xhr.onload = function() {
+   if (xhr.status != 200) {
+       alert(`Ошибка ${xhr.status}: ${xhr.statusText}`)
+   } else {
+       loader.classList.remove('loader_active')
+        let itemsObj = xhr.response;
+            console.log(itemsObj.response.Valute);
+        let itemsArr = Object.values(itemsObj.response.Valute);
+        
+            itemsArr.forEach(elem => {
             let itemElem = document.createElement('div');
             itemElem.className = 'item';
             itemsContainer.append(itemElem);
@@ -36,5 +41,42 @@ xhr.onreadystatechange = function() {
             itemValue.innerHTML = elem.Value;
         });
 
-    }
+   }
 }
+
+
+
+
+
+// xhr.onreadystatechange = function() {
+   
+//     if (xhr.readyState === 4) {
+//         loader.classList.remove('loader_active')
+//         let items = JSON.parse(xhr.responseText).response.Valute;
+//         let itemsArr = Object.values(items);
+//             console.log(Object.values(items))
+//         //    let itemElem = document.querySelector('.item');   
+//         itemsArr.forEach(elem => {
+//             let itemElem = document.createElement('div');
+//             itemElem.className = 'item';
+//             itemsContainer.append(itemElem);
+
+//             let itemCode = document.createElement('div');
+//             itemCode.className = 'item__code';
+//             itemElem.append(itemCode);
+
+//             let itemValue = document.createElement('div');
+//             itemValue.className = 'item__value';
+//             itemElem.append(itemValue);
+
+//             let itemCurr = document.createElement('div');
+//             itemCurr.className = 'item__currency';
+//             itemCurr.innerHTML = 'руб.'
+//             itemElem.append(itemCurr);
+
+//             itemCode.innerHTML = elem.CharCode;
+//             itemValue.innerHTML = elem.Value;
+//         });
+
+//     }
+// }
