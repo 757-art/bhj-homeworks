2ff16d530864faa7c908316bcc435ca0e9788a55
Artem Stepanyuk <aivanov757@gmail.com>
2022-06-23 15:46:46 +0300

    Готовый вариант

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,10 @@
+const editor = document.getElementById('editor');
+    
+    document.addEventListener('keydown', () => {
+    localStorage.text = editor.value;
+    console.log(localStorage.text)
+    }) 
+    
+editor.value= localStorage.text
+
+
